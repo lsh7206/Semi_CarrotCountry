e37a7a124a658dbd3e33048afc985f4302373141
PatrickYoo <dkajffkem@gmail.com>
2020-10-27 23:00:11 +0900

    JDBCtemplate, index.jsp,common folder created

--- CarrotCountry/src/com/javachip/carrotcountry/common/JDBCtemplate.java
+++ CarrotCountry/src/com/javachip/carrotcountry/common/JDBCtemplate.java
@@ -1,11 +1,14 @@
 package com.javachip.carrotcountry.common;
 
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.InvalidPropertiesFormatException;
 import java.util.Properties;
 
@@ -36,8 +39,80 @@ public class JDBCtemplate {
 			e.printStackTrace();
 		}
 		
-		
-		
 		return conn;
+	}
+	
+public static void commit(Connection conn) {
+		
+		try {
+			if(!conn.isClosed()||conn!=null) {
+				
+				conn.commit();
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void rollback(Connection conn) {
+		
+		try {
+			if(!conn.isClosed()||conn!=null) {
+				
+				conn.rollback();
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void close(Connection conn) {
+		try {
+			if(!conn.isClosed()||conn!=null) {
+				
+				conn.close();
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void close(Statement stmt) {
+		
+		try {
+			if(!stmt.isClosed()||stmt!=null) {
+				
+				stmt.close();
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+	}
+	
+	public static void close(ResultSet rs) {
+		
+		try {
+			if(!rs.isClosed()||rs!=null) {
+				
+				rs.close();
+				
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
--- CarrotCountry/src/com/javachip/carrotcountry/member/TemporaryClass.java
+++ CarrotCountry/src/com/javachip/carrotcountry/member/TemporaryClass.java
@@ -3,5 +3,5 @@ package com.javachip.carrotcountry.member;
 public class TemporaryClass {
 	
 	//git 업로드 목적으로 만들어진 class 삭제 요망.
-
+	
 }
